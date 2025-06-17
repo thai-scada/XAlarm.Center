d143c49ba1fc31edf321feb5fed609291952e915
Thanee N. <tn@thaiscada.com>
2025-06-18 00:11:13 +0700

    Implementing...

--- XAlarm.Center.Domain/Events/MessageEvent.cs
+++ XAlarm.Center.Domain/Events/MessageEvent.cs
@@ -1,9 +1,12 @@
 using XAlarm.Center.Domain.Abstractions;
+using XAlarm.Center.Domain.Messages;
 
 namespace XAlarm.Center.Domain.Events;
 
 public sealed class MessageEvent : Entity
 {
+    public Guid ProjectId { get; init; }
+    public AlarmPayload AlarmPayload { get; init; } = new();
     public bool IsSuccess { get; init; }
     public bool IsFailure => !IsSuccess;
     public DateTime EventBeginOnUtc { get; init; }
